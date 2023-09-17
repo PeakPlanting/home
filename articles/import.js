9d45c1dbc79928e39c285f342b36e061eb26ca67
Kristian Temelkov <whatmaneger@gmail.com>
2023-09-17 21:30:41 +0300

    Update import.js

--- articles/import.js
+++ articles/import.js
@@ -17,8 +17,7 @@
 //   }
 // }
 
-function includeHTML(elId, file) {
-  return new Promise((resolve, reject) => {
+async function includeHTML(elId, file) {
     let elmnt, xhttp;
     elmnt = document.querySelector("#" + elId);
 
@@ -28,30 +27,21 @@ function includeHTML(elId, file) {
         if (this.readyState == 4) {
           if (this.status == 200) {
             elmnt.innerHTML += this.responseText;
-            resolve(); // Resolve the promise when the HTML is included.
-          } else {
-            reject("Error loading the page."); // Reject the promise on error.
           }
         }
       };
       xhttp.open("GET", file, true);
       xhttp.send();
-    } else {
-      reject("File not specified."); // Reject the promise if no file is specified.
-    }
-  });
+    } 
 }
 
 async function loadPage() {
   try {
     await includeHTML("article-posts", "articles/zasajdane_na_plodorodni_druvcheta.html");
-    console.log('loaded')
-    // The HTML is now included.
+   
 
-    // Add event listener after the HTML is included.
-      console.log('loaded 2')
-      const button = document.getElementById("showMore");
-      console.log(button);
+    const button = document.getElementById("showMore")
+    console.log(button)
   } catch (error) {
     console.error(error);
   }
