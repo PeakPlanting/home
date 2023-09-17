fc7eb0dc377502347a1a6d0adf44f971872b4cf9
Kristian Temelkov <whatmaneger@gmail.com>
2023-09-17 20:56:12 +0300

    FixImportJs

--- articles/import.js
+++ articles/import.js
@@ -1,21 +1,42 @@
+// function includeHTML(elId, file) {
+//   let elmnt, xhttp;
+//   elmnt = document.querySelector("#"+elId);
+
+//   if (file) {
+//     xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4) {
+//         if (this.status == 200) {elmnt.innerHTML += this.responseText;}
+//         if (this.status == 404) {elmnt.innerHTML += "Page not found.";}
+//         includeHTML();
+//       }
+//     }
+//     xhttp.open("GET", file, true);
+//     xhttp.send();
+//     return;
+//   }
+// }
+
 function includeHTML(elId, file) {
   let elmnt, xhttp;
-  elmnt = document.querySelector("#"+elId);
+  elmnt = document.querySelector("#" + elId);
 
   if (file) {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4) {
-        if (this.status == 200) {elmnt.innerHTML += this.responseText;}
-        if (this.status == 404) {elmnt.innerHTML += "Page not found.";}
-        includeHTML();
+        if (this.status == 200) {
+          elmnt.innerHTML += this.responseText;
+        } else {
+          elmnt.innerHTML += "Error loading the page.";
+        }
       }
-    }
+    };
     xhttp.open("GET", file, true);
     xhttp.send();
-    return;
   }
 }
+
 
 
 function showMore(){
