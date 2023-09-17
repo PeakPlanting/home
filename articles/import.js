9a4c3161f2f88ec1482ee825765a4f47770a4b57
Kristian Temelkov <whatmaneger@gmail.com>
2023-09-17 21:16:54 +0300

    AwatingWholePageRender

--- articles/import.js
+++ articles/import.js
@@ -18,26 +18,43 @@
 // }
 
 function includeHTML(elId, file) {
-  let elmnt, xhttp;
-  elmnt = document.querySelector("#" + elId);
+  return new Promise((resolve, reject) => {
+    let elmnt, xhttp;
+    elmnt = document.querySelector("#" + elId);
 
-  if (file) {
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status == 200) {
-          elmnt.innerHTML += this.responseText;
-        } else {
-          elmnt.innerHTML += "Error loading the page.";
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML += this.responseText;
+            resolve(); // Resolve the promise when the HTML is included.
+          } else {
+            reject("Error loading the page."); // Reject the promise on error.
+          }
         }
-      }
-    };
-    xhttp.open("GET", file, true);
-    xhttp.send();
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+    } else {
+      reject("File not specified."); // Reject the promise if no file is specified.
+    }
+  });
+}
+
+async function loadPage() {
+  try {
+    await includeHTML("article-posts", "articles/zasajdane_na_plodorodni_druvcheta.html");
+    // The HTML is now included.
+
+    // Add event listener after the HTML is included.
+    document.addEventListener("DOMContentLoaded", () => {
+      const button = document.getElementById("showMore");
+      console.log(button);
+    });
+  } catch (error) {
+    console.error(error);
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("showMore")
-  console.log(button)
-})
+loadPage(); // Call the function to start the process.
