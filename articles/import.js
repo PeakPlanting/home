5208fc3fdb5d1223be641a7751dc884c296db5d0
Kristian Temelkov <whatmaneger@gmail.com>
2023-09-17 19:08:00 +0300

    includeHTML

--- articles/import.js
+++ articles/import.js
@@ -1,25 +1,18 @@
+function includeHTML(elId, file) {
+  let elmnt, xhttp;
+  elmnt = document.querySelector("#"+elId);
 
-
-function includeHTML() {
-  var par_div;
-  par_div = document.querySelector("#article-posts");
-  
-  par_div.innerHTML = importText("\articles\zasajdane_na_plodorodni_druvcheta.html")
-}
-
-function importText(file) {
-  var returnText, file, xhttp;
   if (file) {
     xhttp = new XMLHttpRequest();
-    xhttp.open("GET", file, true);
-    xhttp.send();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4) {
-        if (this.status == 200) {returnText = this.responseText;}
-        if (this.status == 404) {returnText = "Page not found.";}
+        if (this.status == 200) {elmnt.innerHTML += this.responseText;}
+        if (this.status == 404) {elmnt.innerHTML += "Page not found.";}
+        includeHTML();
       }
-      return returnText;
     }
-  } 
-  return "Page not found"    
+    xhttp.open("GET", file, true);
+    xhttp.send();
+    return;
+  }
 }
